132249e435a32ad5d25fd2c3d0c64a55b14930e3
Nagy Gergő <nagy.gergi6@gmail.com>
2022-02-26 11:20:02 +0100

    Initial Commit

---
+++ Userlist/UserlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserlistApp.swift
+//  Userlist
+//
+//  Created by Gergo Nagy on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct UserlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
